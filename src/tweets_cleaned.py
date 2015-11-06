6742c5c96fe0f0089bd5f0aa32fb1ecc55b65a54
fkashi <faramarz.kashi@intel.com>
2015-11-05 21:32:54 -0800

    Initial commit

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,27 @@
-# example of program that calculates the number of tweets cleaned
+#!/usr/bin/env python
+import json
+import os
+
+def main():
+    cleaned = 0
+    control = "".join([chr(char) for char in range(0, 31)])  # Removing 0x0000-0x0015
+    escapes = "".maketrans({"\n": " ", "\t": " "})  # Translation table for escape characters
+    infilename = os.path.join("tweet_input", "tweets.txt")
+    outfilename = os.path.join("tweet_output", "ft1.txt")
+    with open(outfilename, mode='w') as outfile:
+        with open(infilename, mode='r') as infile:
+            for line in infile:
+                data = json.loads(line)
+                text = data.get("text")
+                if text is not None:
+                    text_unicode = text.encode('ascii','ignore').decode('UTF-8')
+                    #text_control = text.translate(None,control)
+                    text_escape = text_unicode.translate(escapes)
+                    if len(text) != len(text_escape):
+                        cleaned += 1
+                    output = text_escape + " (timestamp: " + data.get("created_at") + ")\n"
+                    outfile.write(output)
+        outfile.write("\n" + str(cleaned) + " tweets contained unicode.\n")
+
+if __name__ == "__main__":
+    main()
